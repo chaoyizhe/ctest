b9758f49f996fe3c33bcf88a7e136d9c05dfaf57
amiao <2962881160@qq.com>
2024-08-06 21:20:10 +0800

    PRACTICE

---
+++ test8.6-2/test8.6-2/test.c
@@ -0,0 +1,30 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+//int main()
+//{
+//	short age = 18;
+//	int height = 180;
+//	float weight = 60.5;
+//	return 0;
+//}
+//int b = 200; 
+int main()
+{
+	/*short a = 18;
+	int b = 180;
+	float c = 60.5;
+	unsigned int x = a + b + c;
+	printf("x=%d\n", x);*/
+
+	int num1 = 0;
+	int num2 = 0;
+
+	scanf("%d %d",&num1,&num2);
+	int sum = num1 + num2;
+	printf("%d\n",sum);
+
+
+	return 0;
+}
