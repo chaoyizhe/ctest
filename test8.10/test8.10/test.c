91dff768e0d9687b00f7480d0dc51fb0b662dbc1
amiao <2962881160@qq.com>
2024-08-10 13:41:19 +0800

    选择语句与循环语句初识

---
+++ test8.10/test8.10/test.c
@@ -0,0 +1,37 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	int input = 0;
+	printf("你是否同意协定[填1 or 0 ");
+	printf("(1 is yes and 0 is no)]\n");
+	scanf("%d", &input);
+	if (input == 1)
+	{
+		printf("下一步\n");
+	}
+	else
+	{
+		printf("请重试或退出\n");
+	}
+
+	int line = 0;
+	printf("学习C语言");
+
+
+	while(line < 20000)
+	{
+		printf("写代码%d\n", line);
+		line++;
+	}
+	if (line > 20000)
+	{
+		printf("恭喜进入下一阶段");
+	}
+	else
+	{
+		printf("继续加油");
+	}
+	return 0;
+}
